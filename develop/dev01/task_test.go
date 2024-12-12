7905f16619a105634842e1c78e209ad049c3fee9
teodorad <teodorad@student.21-school.ru>
2024-12-12 22:07:19 +0300

    dev01

--- develop/dev01/task_test.go
+++ develop/dev01/task_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTime(t *testing.T) {
+	tt, err := myTime()
+	if err != nil {
+		t.Errorf("Oшибка")
+		return
+	}
+	if tt.Second() != time.Now().Second() {
+		t.Errorf("секунды не совпали")
+	}
+	if tt.Minute() != time.Now().Minute() {
+		t.Errorf("минуты не совпали")
+	}
+	if tt.Hour() != time.Now().Hour() {
+		t.Errorf("часы не совпали")
+	}
+}

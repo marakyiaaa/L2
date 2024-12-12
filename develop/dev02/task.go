aaf50998a29c1fd6bf7075a7604a13abdbf8142b
teodorad <teodorad@student.21-school.ru>
2024-12-12 23:07:34 +0300

    dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -19,5 +24,36 @@ package main
 */
 
 func main() {
+	fmt.Println(unpackingString("222"))
+}
 
+func unpackingString(str string) (string, error) {
+	runes := []rune(str)
+	var res []rune
+	var num int32 = 0
+	var count int32 = 1
+	var j int32
+
+	for i := len(runes) - 1; i >= 0; i-- {
+		if !unicode.IsNumber(runes[i]) { //если значене не цифра
+			for j = 0; j < num-1; j++ {
+				res = append(res, runes[i])
+			}
+			res = append(res, runes[i])
+			num = 0
+			count = 1
+		} else if unicode.IsNumber(runes[i]) {
+			num += int32(runes[i]) * count
+			count *= 10
+			fmt.Println(num, count)
+		}
+	}
+	return reverseString(res), nil
+}
+
+func reverseString(runes []rune) string {
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }

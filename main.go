cce01d27856792f1750f56eb83993c5c8e949da4
teodorad <teodorad@student.21-school.ru>
2025-02-02 21:34:55 +0300

    Factory Method

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"pattern/pattern"
 )
 
@@ -58,21 +59,33 @@ func main() {
 	//// Отмена последней команды (включаем свет обратно)
 	//controller.PressUndo()
 
-	fmt.Println("=== Testing Chain Of Responsibility Pattern ===")
-	spamFilter := &pattern.SpamFilter{}
-	authentication := &pattern.Authentication{}
-	access := &pattern.Authorization{}
-	server := &pattern.Server{}
+	//fmt.Println("=== Testing Chain Of Responsibility Pattern ===")
+	//spamFilter := &pattern.SpamFilter{}
+	//authentication := &pattern.Authentication{}
+	//access := &pattern.Authorization{}
+	//server := &pattern.Server{}
+	//
+	//// Строим цепочку: спам → аутентификация → авторизация → сервер
+	//spamFilter.SetNext(authentication).SetNext(access).SetNext(server)
+	//
+	//fmt.Println("\n --- Администратор отправляет сообщение --- \n")
+	//request1 := pattern.Request{User: "Admin", Status: "admin", Message: "Привет!", IsLoggedIn: true}
+	//spamFilter.Handle(request1)
+	//
+	//fmt.Println(" \n --- Гость без аутентификации --- \n")
+	//request2 := pattern.Request{User: "Guest", Status: "guest", Message: "Привет!", IsLoggedIn: false}
+	//spamFilter.Handle(request2)
 
-	// Строим цепочку: спам → аутентификация → авторизация → сервер
-	spamFilter.SetNext(authentication).SetNext(access).SetNext(server)
+	fmt.Println("=== Testing Factory Method Pattern ===")
+	providers := []string{"paypal", "visa", "master"}
+	randomProvider := providers[rand.Intn(len(providers))]
 
-	fmt.Println("\n --- Администратор отправляет сообщение --- \n")
-	request1 := pattern.Request{User: "Admin", Status: "admin", Message: "Привет!", IsLoggedIn: true}
-	spamFilter.Handle(request1)
+	paymentGet := pattern.GetPaymant(randomProvider)
 
-	fmt.Println(" \n --- Гость без аутентификации --- \n")
-	request2 := pattern.Request{User: "Guest", Status: "guest", Message: "Привет!", IsLoggedIn: false}
-	spamFilter.Handle(request2)
-
+	if paymentGet != nil {
+		fmt.Println("Выбран провайдер:", randomProvider)
+		fmt.Println(paymentGet.ProcessPayment(10010101.50))
+	} else {
+		fmt.Println("Ошибка: Неизвестная платёжная система -", randomProvider)
+	}
 }
